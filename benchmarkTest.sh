2a1f199e77f1ce2244eb239213f86dc5932b5e9d
timoko3 <freepower27@gmail.com>
2026-04-19 00:17:20 +0300

    tried good(pow 2) size for hash table

--- benchmarkTest.sh
+++ benchmarkTest.sh
@@ -1,5 +1,5 @@
 hyperfine -w 5 \
 --min-runs 10 --max-runs 30 \
---export-json benchmarks/hashFuncInlined/comparisonData1.json \
-"taskset -c 2 ./hashTableBenchInlinedHashFunc.out -f test.txt" \
+--export-json benchmarks/goodSizeHashTable/comparisonData1.json \
+"taskset -c 2 ./hashTableBenchGoodSizeHashTable.out -f test.txt" \
 "taskset -c 2 ./hashTableBenchAddrImprovedLFind.out -f test.txt" \
