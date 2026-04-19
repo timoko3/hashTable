61c5731672834ee093adf399a86e75b878dadc72
timoko3 <freepower27@gmail.com>
2026-04-19 19:41:24 +0300

    with other words diaposon measured

--- benchmarkLoadFactor.sh
+++ benchmarkLoadFactor.sh
@@ -1,5 +1,5 @@
-hyperfine -w 5 \
+hyperfine -w 3 \
 --min-runs 10 --max-runs 30 \
---export-json benchmarks/AnalyzeLoadFactorDependence/data1.json \
-"taskset -c 2 ./hashTableBenchLoadFactor.out -f test.txt -c {size}" \
---parameter-list size 200,500,1000,2000,4000,5000,6000,8000,10000,15000,17000,20000,25000,50000
+--export-json benchmarks/AnalyzeLoadFactorDependence/data2.json \
+"taskset -c 2 ./hashTableBench.out -f test.txt -c {size}" \
+--parameter-list size 200,500,1000,4000,5000,10000,15000,20000,25000,50000
